a2f50c48a08d5e2b0cf30f399e0be38debf0fe37
Aeromental.atx* <50771409+IP1241@users.noreply.github.com>
2019-05-29 18:40:32 +0545

    Update whois.py

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -108,12 +108,12 @@ async def fetch_info(replied_user, event):
         "This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
 
-    caption = "<b>USER INFO:</b> \n"
-    caption += f"First Name: {first_name} \n"
-    caption += f"Last Name: {last_name} \n"
-    caption += f"Username: {username} \n"
-    caption += f"ID: <code>{user_id}</code> \n \n"
-    caption += f"Bio: \n<code>{user_bio}</code> \n \n"
+    caption = "<b>USER INFO</b> \n \n"
+    caption += f"__First Name:__ {first_name} \n"
+    caption += f"__Last Name:__ {last_name} \n"
+    caption += f"__Username:__ {username} \n \n"
+    caption += f"<b>ID:</b> <code>{user_id}</code> \n \n"
+    caption += f"<b>BIO:</b> \n<code>{user_bio}</code> \n \n"
     caption += f"Common Chats with this user: {common_chat} \n"
     caption += f"Permanent Link To Profile: "
     caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
